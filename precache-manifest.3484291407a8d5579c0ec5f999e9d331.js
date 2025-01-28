5dd56d6beb83fcd33e7984feb121a0e41a3944e8
Abel Embaye <aembaye@uark.edu>
2025-01-27 22:26:35 -0600

    Updates

---
+++ precache-manifest.3484291407a8d5579c0ec5f999e9d331.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60be6d7c6cb6c25ae833e66d13c52bd1",
+    "url": "/Econ33303-app/index.html"
+  },
+  {
+    "revision": "59aa4388897c24b8e8e1",
+    "url": "/Econ33303-app/static/css/main.046caf2e.chunk.css"
+  },
+  {
+    "revision": "f138d8aab8d1ea6e0e22",
+    "url": "/Econ33303-app/static/js/2.0da98e02.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/Econ33303-app/static/js/2.0da98e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf096f2159482a8b841",
+    "url": "/Econ33303-app/static/js/3.42d73102.chunk.js"
+  },
+  {
+    "revision": "3c3c4848d9d89b74448b7895b74cc4ee",
+    "url": "/Econ33303-app/static/js/3.42d73102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c386216e0ae6bdd003",
+    "url": "/Econ33303-app/static/js/4.0ac62317.chunk.js"
+  },
+  {
+    "revision": "db976c8c39c5dcb11d666b8ac685836a",
+    "url": "/Econ33303-app/static/js/4.0ac62317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30039d557cb63e56a700",
+    "url": "/Econ33303-app/static/js/5.739eb4ae.chunk.js"
+  },
+  {
+    "revision": "036dc6738e2543acd8cf9b1a224d0765",
+    "url": "/Econ33303-app/static/js/5.739eb4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59aa4388897c24b8e8e1",
+    "url": "/Econ33303-app/static/js/main.a5b4cbb3.chunk.js"
+  },
+  {
+    "revision": "200afc139ec106d80cd4",
+    "url": "/Econ33303-app/static/js/runtime-main.4a14c1b5.js"
+  }
+]);
