94c6db09cf6cb42de841b1812a672a74177a7e43
Abel Embaye <aembaye@uark.edu>
2025-03-12 10:35:19 -0500

    committed on 3/12/2025a

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -10,13 +10,13 @@ const Navbar = () => {
         <Bars />
         <NavMenu>
           <NavLink to="/Home">Home</NavLink>
-          <NavLink to="/Act00">Act00</NavLink>
+          {/* <NavLink to="/Act00">Act00</NavLink> */}
           {/* <NavLink to="/ActCh00">Act00</NavLink> */}
           {/* <NavLink to="/ActCh04">Ch4 Activity</NavLink>
           // <NavLink to="/ActCh05">Ch5 Activity</NavLink>*/}
           {/* <NavLink to="/ActCh06">Ch6 Activity</NavLink> */}
           {/* <NavLink to="/Hw01">Homework 1</NavLink> */}
-          <NavLink to="/Pset2">Problem Set 2</NavLink>
+          {/* <NavLink to="/Pset2">Problem Set 2</NavLink> */}
           <NavLink to="/Pset3">Problem Set 3</NavLink>
         </NavMenu>
       </Nav>
